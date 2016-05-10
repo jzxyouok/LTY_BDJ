25ec3986b208b9719477e6c7e69ffdee93ed5303
AYuan <934309587@qq.com>
2016-05-10 22:21:45 +0800

    80使用UILabel做placeholder

--- LTYBDJ/01-百思不得姐/classes/Publish/Controller/LTYPostWordViewController.m
+++ LTYBDJ/01-百思不得姐/classes/Publish/Controller/LTYPostWordViewController.m
@@ -10,7 +10,8 @@
 #import "LTYPlacehorderTextView.h"
 
 @interface LTYPostWordViewController ()
-
+/** 文本输入控件*/
+@property (nonatomic, weak) LTYPlacehorderTextView *textView;
 @end
 
 @implementation LTYPostWordViewController
@@ -19,22 +20,36 @@
     
     [super viewDidLoad];
     
+    [self setupNav];
+    
+    [self setupTextView];
+    
+}
+
+- (void)setupTextView
+{
+    LTYPlacehorderTextView *textView = [[LTYPlacehorderTextView alloc] init];
+    textView.frame = self.view.bounds;
+    textView.placeholder = @"把好玩的图片，好笑的段子或糗事发到这里，接受千万网友膜拜吧！发布违反国家法律内容的，我们将依法提交给有关部门处理。";
+    [self.view addSubview:textView];
+    self.textView = textView;
+    
+}
+
+- (void)setupNav
+{
     self.title = @"发表文字";
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"取消" style:UIBarButtonItemStyleDone target:self action:@selector(cancel)];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"发送" style:UIBarButtonItemStyleDone target:self action:@selector(post)];
     self.navigationItem.rightBarButtonItem.enabled = NO;//默认不能点击
     
-    LTYPlacehorderTextView *textView = [[LTYPlacehorderTextView alloc] init];
-    textView.placeholder = @"哈哈哈哈哈哈哈哈哈哈哈哈哈哈李田圆";
-    self.view = textView;
-    
     
     //强制刷新(有时候设置状态属性但不显示，可能是延迟的问题，则可以强制刷新)
     [self.navigationController.navigationBar layoutIfNeeded];
-//    下面这句不起作用，因为导航控制器与view不在一个层级上，可以拿到导航控制器的view强制刷新
-//    [self.view layoutIfNeeded];
+    //    下面这句不起作用，因为导航控制器与view不在一个层级上，可以拿到导航控制器的view强制刷新
+    //    [self.view layoutIfNeeded];
     
-    
+
     
 }
 
--- LTYBDJ/01-百思不得姐/classes/Publish/View/LTYPlacehorderTextView.m
+++ LTYBDJ/01-百思不得姐/classes/Publish/View/LTYPlacehorderTextView.m
@@ -8,7 +8,28 @@
 
 #import "LTYPlacehorderTextView.h"
 
+@interface LTYPlacehorderTextView()
+
+/** 占位文字label*/
+@property (nonatomic, weak) UILabel *placeholderLabel;
+
+@end
+
 @implementation LTYPlacehorderTextView
+
+- (UILabel *)placeholderLabel
+{
+    if (!_placeholderLabel) {
+        UILabel *placeholderLabel = [[UILabel alloc] init];
+        placeholderLabel.numberOfLines = 0;
+        placeholderLabel.x = 4;
+        placeholderLabel.y = 7;
+        [self addSubview:placeholderLabel];
+        _placeholderLabel = placeholderLabel;
+        
+    }
+    return _placeholderLabel;
+}
 
 //不要让自己成为自己的代理，不然，别人在外面设置代理时自己的代理会被覆盖
 //而且监听占位文字的改变最好不要让控制器作为代理监听，自己内部的事情最好放在内部处理
@@ -20,8 +41,11 @@
         
         //垂直方向上永远有弹簧效果
         self.alwaysBounceVertical = YES;
-        
+        // 默认的占位文字颜色
+        self.placeholderColor = [UIColor grayColor];
+        //默认字体
         self.font = [UIFont systemFontOfSize:15];
+        //监听文字改变
         //设置通知的话，内部接受通知与外部接受通知互不影响
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textDidChange) name:UITextViewTextDidChangeNotification object:nil];
     }
@@ -29,32 +53,48 @@
     return self;
 }
 
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
 /*
  * 监听文字改变
  */
 - (void)textDidChange
 {
-    [self setNeedsDisplay];//将会调用drawRect方法
+    //只要有文字，就隐藏占位文字label
+    self.placeholderLabel.hidden = self.hasText;
 }
 
 /*
  * 绘制占位文字(每次调用drawRect方法之前,系统都会自动把以前画的东西擦掉)
  */
-- (void)drawRect:(CGRect)rect
+//- (void)drawRect:(CGRect)rect
+//{
+//    //如果有文字，直接返回，不绘制占位文字
+////    if (self.text.length || self.attributedText.length) return;
+//    if (self.hasText) return;
+//    
+//    rect.origin.x = 4;
+//    rect.origin.y = 7;
+//    rect.size.width -= 2 * rect.origin.x;
+//    
+//    //文字属性
+//    NSMutableDictionary *attrs = [NSMutableDictionary dictionary];
+//    attrs[NSFontAttributeName] = self.font;
+//    attrs[NSForegroundColorAttributeName] = [UIColor grayColor];
+//    [self.placeholder drawInRect:CGRectMake(rect.origin.x, rect.origin.y, self.width, self.height) withAttributes:attrs];
+//}
+
+/**
+ * 更新占位文字的尺寸
+ */
+
+- (void)updatePlaceholderLabelSize
 {
-    //如果有文字，直接返回，不绘制占位文字
-//    if (self.text.length || self.attributedText.length) return;
-    if (self.hasText) return;
-    
-    rect.origin.x = 4;
-    rect.origin.y = 7;
-    rect.size.width -= 2 * rect.origin.x;
-    
-    //文字属性
-    NSMutableDictionary *attrs = [NSMutableDictionary dictionary];
-    attrs[NSFontAttributeName] = self.font;
-    attrs[NSForegroundColorAttributeName] = [UIColor grayColor];
-    [self.placeholder drawInRect:CGRectMake(rect.origin.x, rect.origin.y, self.width, self.height) withAttributes:attrs];
+    CGSize maxSize = CGSizeMake(LTYScreenW - 2 * self.placeholderLabel.x, MAXFLOAT);
+    self.placeholderLabel.size = [self.placeholder boundingRectWithSize:maxSize options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:self.font} context:nil].size;
+
 }
 
 #pragma mark - 重写setter
@@ -63,35 +103,39 @@
 {
     _placeholder = [placeholder copy];
     
-    [self setNeedsDisplay];
+    self.placeholderLabel.text = placeholder;
+
+    [self updatePlaceholderLabelSize];
 }
 
 - (void)setPlaceholderColor:(UIColor *)placeholderColor
 {
     _placeholderColor = placeholderColor;
     
-    [self setNeedsDisplay];
+    self.placeholderColor  = placeholderColor;
 }
 
 - (void)setFont:(UIFont *)font
 {
     [super setFont:font];
     
-    [self setNeedsDisplay];
+    self.placeholderLabel.font = font;
+    [self updatePlaceholderLabelSize];
+    
 }
 
 - (void)setText:(NSString *)text
 {
     [super setText:text];
     
-    [self setNeedsDisplay];
+    [self textDidChange];
 }
 
 - (void)setAttributedText:(NSAttributedString *)attributedText
 {
     [super setAttributedText:attributedText];
     
-    [self setNeedsDisplay];
+    [self textDidChange];
 }
 
 
