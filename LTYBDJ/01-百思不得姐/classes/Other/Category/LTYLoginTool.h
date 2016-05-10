5f44dad0f41c78bcf9f785b0baa9e77a611b359f
AYuan <934309587@qq.com>
2016-05-11 02:19:41 +0800

    登录实现

---
+++ LTYBDJ/01-百思不得姐/classes/Other/Category/LTYLoginTool.h
@@ -0,0 +1,13 @@
+//
+//  LTYLoginTool.h
+//  01-百思不得姐
+//
+//  Created by AYuan on 16/5/11.
+//  Copyright © 2016年 AYuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LTYLoginTool : NSObject
+
+@end
---
+++ LTYBDJ/01-百思不得姐/classes/Other/Category/LTYLoginTool.m
@@ -0,0 +1,13 @@
+//
+//  LTYLoginTool.m
+//  01-百思不得姐
+//
+//  Created by AYuan on 16/5/11.
+//  Copyright © 2016年 AYuan. All rights reserved.
+//
+
+#import "LTYLoginTool.h"
+
+@implementation LTYLoginTool
+
+@end
