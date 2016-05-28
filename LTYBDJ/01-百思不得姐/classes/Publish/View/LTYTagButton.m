4e09cbbd08b0df61d971eb540af93f9ca6009462
AYuan <934309587@qq.com>
2016-05-29 00:11:36 +0800

    封装标签按钮

---
+++ LTYBDJ/01-百思不得姐/classes/Publish/View/LTYTagButton.m
@@ -0,0 +1,21 @@
+//
+//  LTYTagButton.m
+//  01-百思不得姐
+//
+//  Created by AYuan on 16/5/28.
+//  Copyright © 2016年 AYuan. All rights reserved.
+//
+
+#import "LTYTagButton.h"
+
+@implementation LTYTagButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
