da4a9e25cef097816af79a60f847af67a3948339
AYuan <934309587@qq.com>
2016-05-28 15:59:53 +0800

    标签按钮的添加和删除

--- LTYBDJ/01-百思不得姐/classes/Publish/Controller/LTYAddTargetViewController.m
+++ LTYBDJ/01-百思不得姐/classes/Publish/Controller/LTYAddTargetViewController.m
@@ -16,10 +16,22 @@
 @property (nonatomic, weak) UIButton *targetBtn;
 /** 文本输入框*/
 @property (nonatomic, weak) UITextField *textField;
+/** 标签数组 */
+@property (nonatomic, strong) NSMutableArray *tagArray;
+
+
 
 @end
 
 @implementation LTYAddTargetViewController
+
+- (NSMutableArray *)tagArray
+{
+    if (_tagArray == nil) {
+        _tagArray = [NSMutableArray array];
+    }
+    return _tagArray;
+}
 
 - (UIButton *)targetBtn
 {
@@ -29,7 +41,7 @@
         targetBtn.width = self.contentView.width;
         targetBtn.height = 35;
         [targetBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        [targetBtn addTarget:self action:@selector(targetBtnClick) forControlEvents:UIControlEventTouchUpInside];
+        [targetBtn addTarget:self action:@selector(addTargetBtn) forControlEvents:UIControlEventTouchUpInside];
         targetBtn.titleLabel.font = [UIFont systemFontOfSize:14];
         targetBtn.contentEdgeInsets = UIEdgeInsetsMake(0, LTYTargetMargin, 0, LTYTargetMargin);
         // 让按钮内部的文字和图片都左对齐
@@ -42,18 +54,83 @@
     return _targetBtn;
 }
 
-- (void)targetBtnClick
+/*
+ * 添加标签按钮
+ */
+- (void)addTargetBtn
 {
-    NSLog(@"%s",__func__);
+    //添加一个标签按钮
     UIButton *tabButton = [UIButton buttonWithType:UIButtonTypeCustom];
     [tabButton setImage:[UIImage imageNamed:@"chose_tag_close_icon"] forState:UIControlStateNormal];
     [tabButton setTitle:self.textField.text forState:UIControlStateNormal];
     [tabButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [tabButton setBackgroundColor:LTYTabBg];
     [tabButton sizeToFit];
+    [tabButton addTarget:self action:@selector(tagButtonClick:) forControlEvents:UIControlEventTouchUpInside];
     [self.contentView addSubview:tabButton];
+    [self.tagArray addObject:tabButton];
+    
+    //更新标签按钮的frame
+    [self updateTagButtonFrame];
+    
+    //清空textfield文字
     self.textField.text = nil;
+    self.targetBtn.hidden = YES;
+    
 }
+
+/*
+ * 专门用来更新标签按钮的frame
+ */
+- (void)updateTagButtonFrame
+{
+    
+    for (int i = 0; i < self.tagArray.count; i++) {
+    
+        UIButton *tagButton = self.tagArray[i];
+        if (i == 0) { //第一个标签按钮
+            tagButton.x = 0;
+            tagButton.y = 0;
+        } else { //其他标签按钮
+            UIButton *lastTagButton = self.tagArray[i - 1];
+            //当前行左边的宽度
+            CGFloat leftWidth = CGRectGetMaxX(lastTagButton.frame) + LTYTargetMargin;
+            //计算当前行右边的宽度
+            CGFloat rightWidth = self.contentView.width - leftWidth;
+            if (rightWidth >= tagButton.width) { //按钮显示在当前行
+                tagButton.x = CGRectGetMaxX(lastTagButton.frame) + LTYTargetMargin;
+                tagButton.y = lastTagButton.y;
+            }else { //按钮显示在下一行
+                tagButton.x = 0;
+                tagButton.y = CGRectGetMaxY(lastTagButton.frame) + LTYTargetMargin;
+            }
+        }
+
+    }
+    
+    
+    //更新textField的frame
+    self.textField.x = 0;
+    self.textField.y = CGRectGetMaxY([[self.tagArray lastObject] frame]) + LTYTargetMargin;
+    
+}
+
+/*
+ * 标签按钮的点击
+ */
+- (void)tagButtonClick:(UIButton *)tagButton
+{
+    [tagButton removeFromSuperview];
+    [self.tagArray removeObject:tagButton];
+    //更新所有标签按钮的frame
+    [UIView animateWithDuration:0.25 animations:^{
+        [self updateTagButtonFrame];
+    }];
+    
+}
+
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -76,6 +153,7 @@
     [textField becomeFirstResponder];
     [self.contentView addSubview:textField];
     self.textField = textField;
+    
 }
 
 - (void)textDidChange
